15657538fc9bb7348efd65a91abb93a3474ed4ab
Das786 <56470462+Das786@users.noreply.github.com>
2022-04-09 15:02:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 if('serviceWorker' in navigator){
     window.addEventListener('load', function(){
-        navigator.serviceWorker.register('/sw.js').then(function (registration){
+        navigator.serviceWorker.register('/PWA/sw.js').then(function (registration){
             console.log('serviceWorker registration successful with scope', registration.scope);
         }, function (err) {
             console.log("ServiceWorker registration failed",err);
